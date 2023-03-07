4da3f0eb691399245ed5f1db19bcd867a335b6d1
Guillaume Cazals <gcazals06@gmail.com>
2023-03-07 16:01:38 +0100

    controllers and show page, work in progress

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,12 @@
 class CategoriesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @questions = @category.questions
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,4 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home]
 
-  def index
-    @categories = Category.all
-  end
-
-  def show
-    @category = Category.find(params[:id])
-    @questions = @category.questions
-  end
 end
