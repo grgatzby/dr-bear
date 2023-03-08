6dfa941dd513ce8d5537bebb498ef139f4bbb6a2
Ian Kelly <idk13@hotmail.com>
2023-03-08 11:26:56 +0100

    added description text field to categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_132717) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_102358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_132717) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "category_nutrients", force: :cascade do |t|
