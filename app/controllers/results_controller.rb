74cc94755d1f6baac9c8ad03490a8c1c3b7af7f5
Ian Kelly <idk13@hotmail.com>
2023-03-08 16:03:47 +0100

    working on result controller show method

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,27 @@
 class ResultsController < ApplicationController
   def index
-    @results = Result.all
+
+  end
+
+  def show
+    cat_nutrients = CategoryNutrient.all
+    result = result.find()
+    cat_nutrients.where(category_id: result.category_id).nutrients
+
+
+    &&
+    cat_nutrients.where(min_score:  result.total_score) &&
+    cat_nutrients.where(max_score:  result.total_score) &&
+
+
+    @cat_nutri.each do |element|
+      nutrient = []
+      if element.category_id == result.category_id
+        nutrient << element.nutrient_id
+      end
+
+
+
   end
 
 end
