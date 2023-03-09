4c4dbeb824a6f5c5bcc42f8a4bf55fc9d4a5d34d
Guillaume Cazals <gcazals06@gmail.com>
2023-03-09 15:23:41 +0100

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,4 +9,5 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :show, :create]
 
   resources :results, only: [:show]
+  get "results", to: "pages#home"
 end
