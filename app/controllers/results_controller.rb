74e8e9611f2d80b692969e2e34ef4530fdb88ce2
Ian Kelly <idk13@hotmail.com>
2023-03-09 10:51:25 +0100

    completed result retrieving nutrient per category

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,12 +2,11 @@ class ResultsController < ApplicationController
 
   def index
     cat_nutrients = CategoryNutrient.all
-    result = Result.find(9)
+    result = Result.find(7)
     @category = Category.find(result.category_id)
 
     @nutrient = []
     cat_nutrients.where(category_id: result.category_id).where("min_score <= ?", result.total_score).where("max_score >= ?", result.total_score).each { |element| @nutrient << element.nutrient_id }
-
 
   end
 
