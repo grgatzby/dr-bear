8b00a617b486ef5bc07762f144754bbe22ce9b0d
Guillaume Cazals <gcazals06@gmail.com>
2023-03-10 15:46:33 +0100

    duplicate on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  get "results", to: "pages#home"
-
-  get "about", to: "pages#about"
-  get "learn", to: "pages#learn"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
@@ -13,6 +8,10 @@ Rails.application.routes.draw do
 
   resources :results, only: [:show]
 
+  get "results", to: "pages#home"
+
+  get "about", to: "pages#about"
+  get "learn", to: "pages#learn"
   get "results", to: "pages#home"
   get "results/:id/basket", to: "results#basket", as: "basket"
   patch "results/:id", to: "results#update"
