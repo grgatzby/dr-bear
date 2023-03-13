ebb6e225ac9bc01d918d66377a3c748c314a7994
Guillaume Cazals <gcazals06@gmail.com>
2023-03-13 10:44:43 +0100

    multi select cards in categories/index.html and associated code in the controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,5 @@
 class CategoriesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show, :create, :multishow]
-  $all_cat_results = {}
-  $show_params = {}
 
   def index
     @categories = Category.all
@@ -11,62 +9,46 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @questions = @category.questions
     @results = params[:result]
-    # raise
-    # render :show
-    # redirect_to categories_path(params[:id])
-    $show_params = params
   end
 
   def multishow
-    @categories = Category.select{ |category| params[category.id.to_s] }
-    current_params = params
+    @categories = Category.select { |category| params[category.id.to_s] }
     @categories.each do |cat|
       @category = cat
+      @questions = @category.questions
       cat_results = {}
       cat_results[:category_id] = @category.id
       cat_results[:category_name] = @category.name
       @questions = @category.questions
       params[:id] = @category.id
       # go to show page (this category quiz)
-      show
-      cat_results[:category_result] = $show_params[:result]
-      raise
-      @results = $show_params[:result]
-      $all_cat_results [:category_id] = @results
     end
     # redirect_to category_path(@cat_id) unless params[:id]
+    @results = params[:result]
   end
 
   def create
     #gets the results from the questions form
     # params["cat[:id]q[:id]"] => score for this category_id/question_id, in string format
-    raise
     if params[:category]
-      @categories = Category.find(params[:category].to_i)
-      @categories.each do |cat|
-        @cat_id = cat.id
-        if params[@cat_id.to_s]
-          @category = cat
-          @questions = @category.questions
-          @max_category_score = 0
-          @category_score = 0
-          @questions.each do |question|
-            @category_score += params["cat#{params[:category]}q#{question.id}"].to_i
-            max_question_score = question.answers.map{ |answer| answer.score }.max
-            @max_category_score += max_question_score
-          end
-          # current user if logged in, dummy user if not
-          user_to_attach = current_user || User.find_by(first_name: "Crash", last_name: "Dummy")
-          @category_result = Result.create!(
-            total_score: @category_score,
-            max_score: @max_category_score,
-            user_id: user_to_attach.id,
-            category_id: @category.id
-          )
-        end
+      @category = Category.find(params[:category].to_i)
+      @questions = @category.questions
+      @max_category_score = 0
+      @category_score = 0
+      @questions.each do |question|
+        @category_score += params["cat#{params[:category]}q#{question.id}"].to_i
+        max_question_score = question.answers.map{ |answer| answer.score }.max
+        @max_category_score += max_question_score
       end
+      user_to_attach = current_user || User.find_by(first_name: "Crash", last_name: "Dummy")
+      @category_result = Result.create!(
+        total_score: @category_score,
+        max_score: @max_category_score,
+        user_id: user_to_attach.id,
+        category_id: @category.id
+      )
+      # show results
+      redirect_to result_path(@category_result.id)
     end
-    # show results
-    redirect_to result_path(@category_result.id)
   end
 end
