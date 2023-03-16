05832238eab704e10cc69f3563bfeb3e0a378b5a
Ian Kelly <idk13@hotmail.com>
2023-03-16 13:00:28 +0100

    popover update with click anywhere to hide & began styling

--- app/javascript/controllers/popover_controller.js
+++ app/javascript/controllers/popover_controller.js
@@ -4,5 +4,8 @@ import { Popover } from "bootstrap/dist/js/bootstrap.bundle.js";
 export default class extends Controller {
   connect() {
     new Popover(this.element)
+    var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+      trigger: 'focus'
+    })
   }
 }
